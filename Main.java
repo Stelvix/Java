c2f924aead8fb90b2b050439486000ddb839ed82
Steeven HOUNKPE-SAGBO <e.hounkpesagbo@ecoles-epsi.net>
2026-03-18 15:41:33 +0100

    Mini gestionnaire d'utilisateur

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import java.util.List;
 import java.util.jar.Attributes.Name;
 import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Main {
 
@@ -14,6 +15,24 @@ public class Main {
 
         for (String produit : listeDeCourse) {
             System.out.println(produit);
+        }
+
+        // une autre liste
+
+        List<String> MaStack = new ArrayList<>(Arrays.asList("Symfony", "Javascript", "C#"));
+        System.out.println("Ancienne liste");
+        System.out.println(MaStack);
+
+        List<String> OtherStacks = new ArrayList<>(Arrays.asList("bla", "bla", "bla", "ospvjpvs"));
+        for (String other : OtherStacks) {
+
+            MaStack.add(other);
+            System.out.println("Ajout dynamique des autres stacks à la list MaStack, nouvelle liste");
+
+            for (String item : MaStack) {
+                System.out.println(item);
+            }
+
         }
 
         System.out.println("-------- La boucle FOR -------");
@@ -44,5 +63,28 @@ public class Main {
         User u2 = new User("Chris", 30, "Je suis achitècte cloud chez AWS(Amazone Web Service)");
         u2.sayHello();
 
+        // ---------- user hub ----------
+
+        List<String> UsersHub = new ArrayList<>(Arrays.asList("Paul", "Franc", "Marc", "François"));
+        List<Integer> UsersAges = new ArrayList<>(Arrays.asList(20, 12, 30, 56));
+        List<String> UsersDescriptions = new ArrayList<>(Arrays.asList("Je suis techicien de surface",
+                "Administrateur Système et réseau", "DevOps chez Onepoint", "DevSecOps chez CapGemini"));
+
+        System.out.println(UsersHub.size());
+
+        for (int i = 0; i < UsersHub.size(); i++) {
+            User user = new User(UsersHub.get(i), UsersAges.get(i), UsersDescriptions.get(i));
+            user.sayHello();
+        }
+
+        /*
+         * // -- Compte à rebours --
+         * 
+         * System.out.println("Le compte à rebours");
+         * 
+         * for (int c = 0; c < 10; c++) {
+         * System.out.println(c);
+         * }
+         */
     }
 }
